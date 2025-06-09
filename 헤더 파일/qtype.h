7874e495b7e3b86b14dc54eda9544de4a1800976
박완규 <162093749+ohnakyu@users.noreply.github.com>
2025-06-09 14:37:21 +0000

    key, value + value_size 추가

--- 헤더 파일/qtype.h
+++ 헤더 파일/qtype.h
@@ -1,33 +1,32 @@
-#ifndef _QTYPE_H  // header guard
+#ifndef _QTYPE_H
 #define _QTYPE_H
 
-// ==========이 파일은 수정 가능==========
+#include <atomic>
+#include <mutex>
 
-typedef unsigned int Key;  // 값이 클수록 높은 우선순위
+typedef unsigned int Key;   // 값이 클수록 높은 우선순위
 typedef void* Value;
 
+// Item 구조체: 깊은 복사를 위한 value_size 필드 포함
 typedef struct {
     Key key;
     Value value;
+    int value_size;  // 바이트 단위의 value 크기
 } Item;
 
+// 연산 결과를 담는 구조체
 typedef struct {
-    bool success;   // true: 성공, false: 실패
+    bool success;  // true: 성공, false: 실패
     Item item;
-    // 필드 추가 가능
 } Reply;
 
+// (Linked list 관련 함수는 사용하지 않지만 정의는 유지)
 typedef struct node_t {
     Item item;
     struct node_t* next;
-    // 필드 추가 가능
 } Node;
 
-typedef struct {
-    Node* head, tail;
-    // 필드 추가 가능
-} Queue;
-
-// 이후 자유롭게 추가/수정: 새로운 자료형 정의 등
+// Queue 구조체는 queue.cpp에서 reinterpret_cast<QueueImpl*>로 구현됨
+typedef struct Queue Queue;
 
 #endif
